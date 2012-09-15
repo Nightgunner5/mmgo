ae0b15284923572692477f8fb8023b29c2853a17
Ben L <nightgunner5@llamaslayers.net>
2012-09-15 11:48:03 -0500

    now with less white

--- ui/menu.go
+++ ui/menu.go
@@ -9,11 +9,20 @@ import (
 func DisplayMenu() {
 	gl.Clear(gl.COLOR_BUFFER_BIT)
 	gl.Begin(gl.QUADS)
+
 	gl.Color3f(1, 1, 1)
 	gl.Vertex2f(-0.6, -0.8)
 	gl.Vertex2f(+0.6, -0.8)
 	gl.Vertex2f(+0.6, +0.8)
 	gl.Vertex2f(-0.6, +0.8)
+
+	gl.Color3f(0, 0, 0)
+	gl.Vertex2f(-0.55, -0.75)
+	gl.Vertex2f(+0.55, -0.75)
+	gl.Color3f(0.2, 0.2, 0.2)
+	gl.Vertex2f(+0.55, +0.75)
+	gl.Vertex2f(-0.55, +0.75)
+
 	gl.End()
 	gl.Flush()
 }
@@ -22,6 +31,7 @@ func ReshapeMenu(width, height int) {
 	gl.Viewport(0, 0, width, height)
 	gl.MatrixMode(gl.PROJECTION)
 	gl.LoadIdentity()
+
 	aspect := float64(width) / float64(height)
 	w, h := 1.0, 1.0
 	if aspect > 1 {
